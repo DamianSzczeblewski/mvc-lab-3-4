450a83f69128e206c1b87681a8baa2a6f4537ba0
DamianSzczeblewski <163163276+DamianSzczeblewski@users.noreply.github.com>
2024-04-09 18:16:22 +0200

    Update students.js

--- zad2/controllers/students.js
+++ zad2/controllers/students.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const path = require("path");
 
-const router = express.Router();
-
-const getAddNewStudentPage = router.get("/", (request, response, next) => {
+exports.getAddNewStudentPage = (request, response, next) => {
     response.render("Home", {});
-});
-
-module.exports = getAddNewStudentPage;
+};
